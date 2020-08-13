6f158a1204e084e5fb08f93d6ae7401fe28f9331
kyochina <46920785+kyochina@users.noreply.github.com>
2020-08-14 01:57:14 +0800

    Add files via upload

---
+++ ecar/src/main/java/com/MainContronler.java
@@ -0,0 +1,7 @@
+package com;
+
+public class MainContronler {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
---
+++ ecar/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!  student spring mvc start</h2>
+</body>
+</html>
