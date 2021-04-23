0735047b5be3116035ef19c38398da29e9541625
kyochina <terry_china@163.com>
2021-04-23 16:47:36 +0800

    文件转base64字符串

---
+++ ecar/src/main/java/com/china/utils/http/FileToBase64.java
@@ -0,0 +1,52 @@
+public static String file2Base64(File file) {
+        if(file==null) {
+            return null;
+        }
+        String base64 = null;
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream(file);
+            byte[] buff = new byte[fin.available()];
+            fin.read(buff);
+            base64 = Base64.encode(buff);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fin != null) {
+                try {
+                    fin.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return base64;
+    }
+
+
+public static File base64ToFile(String base64) {
+        if(base64==null||"".equals(base64)) {
+            return null;
+        }
+        byte[] buff=Base64.decode(base64);
+        File file=null;
+        FileOutputStream fout=null;
+        try {
+            file = File.createTempFile("tmp", null);
+            fout=new FileOutputStream(file);
+            fout.write(buff);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(fout!=null) {
+                try {
+                    fout.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return file;
+    }
