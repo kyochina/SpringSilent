99dc8b116c2262de94e3922606dd7d6069419217
kyochina <terry_china@163.com>
2021-04-29 16:53:22 +0800

    增加JAVA8 Stream demo

---
+++ ecar/src/main/java/com/china/utils/http/FilterTest.java
@@ -0,0 +1,24 @@
+package com.javaOn8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilter {
+	public static void main(String[] args) {
+		List<String> arr = new ArrayList<String>();
+		List<String> out = new ArrayList<String>();
+		arr.add("111");
+		arr.add("2222");
+		arr.add("33333");
+		arr.add("444444");
+		arr.add("11");
+		arr.add("00");
+		out = arr.stream().filter(str -> str.startsWith("11")).collect(Collectors.toList());
+		
+		out.forEach(str -> System.out.println(str));
+		
+		arr.stream().filter(str -> str.length()>3).forEach(str -> System.out.println(str));
+		
+	}
+}
